184d3f4eed8efbf8ff48fd1486f1a7eb69bcb461
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-24 13:57:24 -0400

    exported remove and added comments

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -9,10 +9,20 @@ export const todosSlice = createSlice({
     {
         create: (state, action) =>
         {
+            // can't do this without toolkit using Immer
             state.todosData.push(action.payload)
+            // without toolkit, would need to return like this to avoid mutation
+            // return {
+            //     ...state,
+            // /    todosData: [...state.todosData, action.payload]
+            // }
+        },
+        remove: (state, action) =>
+        {
+            state.todosData = state.todosData.filter(todo => todo.id !== action.payload)
         }
     }
 })
 
-export const { create } = todosSlice.actions
+export const { create, remove } = todosSlice.actions
 export default todosSlice.reducer
